a8f225267942e4a1785f215c214c54c0fe643e8c
Pierluigi Petrelli <pier@PierluigisMBP3.biscottino.local>
2019-08-15 02:29:13 +0200

    more control to the watering setup

--- moisture_arduino.ino
+++ moisture_arduino.ino
@@ -18,43 +18,66 @@ JsonObject sensor_3 = sensors.createNestedObject();
 JsonObject sensor_4 = sensors.createNestedObject();
 JsonObject sensor_5 = sensors.createNestedObject();
 
-Timer<15> timer;
-
-static const uint8_t analog_pins[] = {A0, A1, A2, A3, A4, A5};
+Timer<25> timer;
 
               //A0  A1   A2   A3   A4   A5 
-int air[]   = { 767, 705, 775, 777, 776, 703 };
-int water[] = { 460, 439, 466, 477, 466, 426 };
-int min_moisture[] = { 40, 40, 40, 40, 40, 40 };
-int pump_pins[] = { 4, 5, 6, 7};
+static const uint8_t analog_pins[] = {A0, A1, A2, A3, A4, A5};
+int air[]   = { 663, 670, 775, 777, 776, 703 };
+int water[] = { 356, 359, 466, 477, 466, 426 };
+int moist_water_trigger[] = { 40, 40, 40, 40, 40, 40 };
+int moist_water_stop[] = { 65, 65, 65, 65, 65, 65 };
+int watering_time[] = { 2, 2, 7, 7, 7, 7 };
+int pump_pins[] = { 4, 5, 6, 7 };
 
-int latest_moisture_perc[] = {0,0,0,0,0,0};
+bool grass_triggered[] = {false, false, false, false, false, false};
+int latest_moisture_perc[] = { 0,0,0,0,0,0 };
+bool sensors_reading_disabled=false; //while the pump is on the interference falses the values
 
 // this is a workaround for some sensors
 int ground_pin = 8;
 
-bool reset_pump(void *pump_index) {
+bool stop_pump(void *pump_index) {
+  //Serial.print("closing=");Serial.println((int)pump_index);
   digitalWrite(pump_pins[(int)pump_index], LOW); 
+  sensors_reading_disabled=false;
   return true;   
 }
 
 bool water_manager(void *) {
-
+  
   for (int i = 0; i < 6; i++) { 
-
-    if (latest_moisture_perc[i] < min_moisture[i])  {
-        digitalWrite(pump_pins[i], HIGH);
-        timer.in(7000, reset_pump, (void *)i);
-      }
+    // If I add some serial.print here it basically breaks the code and I have no clue why
+    //Serial.print("grass=");Serial.println(i);  
+    //Serial.print("latest_moisture_perc=");Serial.println(latest_moisture_perc[i]);
+    //Serial.print("moist_water_trigger=");Serial.println(moist_water_trigger[i]);
+    //Serial.print("grass_triggered=");Serial.println(grass_triggered[i]);
+   
+    if ( (latest_moisture_perc[i] < moist_water_trigger[i]) && !grass_triggered[i])  { 
+      grass_triggered[i]=true;
+      sensors_reading_disabled=true;
+      digitalWrite(pump_pins[i], HIGH);
+      timer.in(watering_time[i]*1000, stop_pump , (void *)i);
+    }
+    else if ( ( latest_moisture_perc[i] <= moist_water_stop[i] ) && grass_triggered[i] ) {  
+      sensors_reading_disabled=true;
+      digitalWrite(pump_pins[i], HIGH);
+      timer.in(watering_time[i]*1000, stop_pump, (void *)i);
+    }
+    else if ( ( latest_moisture_perc[i] >= moist_water_stop[i] ) && grass_triggered[i] ) {  
+      grass_triggered[i]=false;
+    }
 
   }
-  
+
+  return true;
 }
 
 bool read_values(void *) {
 
   //char jsonOutput[500];
-
+  if (sensors_reading_disabled)
+    return true;
+    
   for (int i = 0; i < 6; i++) { 
 
     int moisture_raw = analogRead(analog_pins[i]);
@@ -93,17 +116,6 @@ bool sensors_reset(void *) {
   
 }
 
-
-void read_serial() {
-
-  /*if (mySerial.available()) {
-    String msg = mySerial.readString();
-    Serial.print("Data received: ");
-    Serial.println(msg);
-  } */
-  
-}
-
 void pin_setup() {
    
   pinMode(ground_pin, OUTPUT); 
@@ -130,16 +142,11 @@ void setup() {
 
   timer.every(30000, sensors_reset);
   timer.every(35000, water_manager);
-
-  //water_manager(,i);
-  
-  //Serial.println("Setup completed");
   
 }
 
 void loop() {
 
-    timer.tick();
-  //read_serial();
+  timer.tick();
   
 }
