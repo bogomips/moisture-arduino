959946e37d1b82f7fd54d7d22981829546e82755
Pierluigi Petrelli <pierluigi@petrelli.biz>
2019-08-26 01:13:56 +0200

    clean up

--- moisture_arduino.ino
+++ moisture_arduino.ino
@@ -8,7 +8,6 @@
 //SoftwareSerial mySerial(0, 1); // RX, TX on Arduino
 #define SERIAL_BUFFER_SIZE 500
 
-
 StaticJsonDocument<500> http_response;
 JsonArray sensors = http_response.createNestedArray("sensors");
 JsonObject sensor_0 = sensors.createNestedObject();
@@ -27,13 +26,12 @@ int water[] = { 344, 355, 355, 364, 317, 317 };
 int moist_water_trigger[] = { 40, 40, 40, 40, 40, 40 };
 int moist_water_stop[] = { 65, 65, 65, 65, 65, 65 };
 int watering_time[] = { 2, 2, 7, 7, 7, 7 };
-//int pump_pins[] = { 4, 5, 6, 7 };
 
 bool grass_triggered[] = {false, false, false, false, false, false};
 int latest_moisture_perc[] = { 0,0,0,0,0,0 };
 bool sensors_reading_disabled=false; //while the pump is on the interference falses the values
 
-// this is a workaround for some sensors
+//this is a workaround for some sensors
 //int ground_pin = 8;
 
 // shift register
@@ -41,11 +39,10 @@ int clockPin = 4 ; //blue
 int dataPin  = 7; //yellow
 int latchPin  = 8; //violet
 
+//The relay modules works with an inverted logic
 int relay_on = LOW;
 int relay_off = HIGH;
 int relaysRegister[] = { relay_off, relay_off, relay_off, relay_off, relay_off, relay_off, relay_off, relay_off };
-
-
 
 void updateShiftRegister() {
 
@@ -55,7 +52,6 @@ void updateShiftRegister() {
   shiftOut(dataPin, clockPin, LSBFIRST, test); 
   digitalWrite(latchPin, HIGH);*/
 
-  //digitalWrite(clockPin,LOW);
   digitalWrite(latchPin,LOW);
 
   for (int i = 0; i < 8; i++) {
@@ -69,8 +65,7 @@ void updateShiftRegister() {
 }
 
 bool stop_pump(void *pump_index) {
-  //Serial.print("closing=");Serial.println((int)pump_index);
-  //digitalWrite(pump_pins[(int)pump_index], relay_off); 
+ 
   relaysRegister[(int)pump_index] = relay_off;
   updateShiftRegister();
   sensors_reading_disabled=false;
@@ -78,19 +73,17 @@ bool stop_pump(void *pump_index) {
 }
 
 bool start_pump(void *pump_index) {
+
   sensors_reading_disabled=true;
   relaysRegister[(int)pump_index] = relay_on;
   updateShiftRegister();
+
 }
 
 bool water_manager(void *) {
   
   for (int i = 0; i < 6; i++) { 
     // If I add some serial.print here it basically breaks the code and I have no clue why
-    //Serial.print("grass=");Serial.println(i);  
-    //Serial.print("latest_moisture_perc=");Serial.println(latest_moisture_perc[i]);
-    //Serial.print("moist_water_trigger=");Serial.println(moist_water_trigger[i]);
-    //Serial.print("grass_triggered=");Serial.println(grass_triggered[i]);
    
     if ( (latest_moisture_perc[i] < moist_water_trigger[i]) && !grass_triggered[i])  { 
       grass_triggered[i]=true;
@@ -180,17 +173,13 @@ void setup() {
     
   }
 
-
  //timer.every(30000, sensors_reset);
   timer.every(30000, read_values);
   timer.every(35000, water_manager);
 
-  
 }
 
 void loop() {
 
   timer.tick();
-
-  
 }
